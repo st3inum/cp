926cafd57e3db6b9e146895e0e0a7d396974ae8b
st3inum <st3inum@gmail.com>
2019-07-12 02:49:39 +0600

    Add files via upload

--- kmp.cpp
+++ kmp.cpp
@@ -3,36 +3,35 @@ using namespace std;
 
 
 //kmp
-const static int mx=1000010;
-char s[mx],ss[mx];
+const static int mx = 1000010;
+char s[mx], ss[mx];
 int lps[mx];
-void build_lps()
+void build_lps(int n)
 {
-	int n=strlen(s);
-	lps[0]=0;
-	for(int i=1;i<n;i++)
+	lps[0] = 0;
+	for (int i = 1; i < n; i++)
 	{
-		int j=lps[i-1];
-		while(j>0 && s[i]!=s[j])j=lps[j-1];
-		if(s[i]==s[j])j++;
-		lps[i]=j;
+		int j = lps[i - 1];
+		while (j > 0 && s[i] != s[j])j = lps[j - 1];
+		if (s[i] == s[j])j++;
+		lps[i] = j;
 	}
 }
-int find_occ(int n,int m)
+int find_occ(int n, int m) //pattern length, text length
 {
-	build_lps();
-	int i=0,j=0,c=0;
-	while(j<m)
+	build_lps(n);
+	int i = 0, j = 0, c = 0;
+	while (j < m)
 	{
-		if(s[i]==ss[j])
+		if (s[i] == ss[j])
 		{
-			i++,j++;
-			if(i==n)c++,i=lps[i-1];
+			i++, j++;
+			if (i == n)c++, i = lps[i - 1];
 		}
 		else
 		{
-			if(i==0)j++;
-			else i=lps[i-1];
+			if (i == 0)j++;
+			else i = lps[i - 1];
 		}
 	}
 	return c;
@@ -41,12 +40,12 @@ int find_occ(int n,int m)
 int main(int argc, char const *argv[])
 {
 	int t;
-	scanf("%d",&t);
-	for(int i=1;i<=t;i++)
+	scanf("%d", &t);
+	for (int i = 1; i <= t; i++)
 	{
-		printf("Case %d: \n",i);
-		scanf("%s %s",ss,s);
-		printf("%d\n", find_occ(strlen(s),strlen(ss)));
+		printf("Case %d: \n", i);
+		scanf("%s %s", ss, s);
+		printf("%d\n", find_occ(strlen(s), strlen(ss)));
 	}
 	return 0;
 }
