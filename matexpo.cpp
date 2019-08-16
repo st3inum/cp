b73cf9bd97731c021693719cb47c3884bc2b1f01
st3inum <st3inum@gmail.com>
2019-08-17 00:48:32 +0600

    Add files via upload

--- matexpo.cpp
+++ matexpo.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef unsigned long long ull;
+
 template<class T>
 class matrix
 {
-	typedef vector<vector<ull>> mat;
+	typedef vector<vector<T>> mat;
 public:
 	mat I;
 	matrix() {}
 	matrix(int n) {
-		vector<ull> v(n, 0);
+		vector<T> v(n, 0);
 		for (int i = 0; i < n; i++)
 		{
 			v[i] = 1;
@@ -19,7 +19,7 @@ public:
 	}
 	matrix(int r, int c, int val)
 	{
-		I.resize(r, vector<ull> (c, val));
+		I.resize(r, vector<T> (c, val));
 	}
 	matrix(mat v)
 	{
@@ -41,7 +41,7 @@ public:
 		matrix temp;
 		for (int i = 0; i < I.size(); i++)
 		{
-			vector<ull> v;
+			vector<T> v;
 			for (int j = 0; j < I[0].size(); j++)
 			{
 				v.push_back(I[i][j] + obj.I[i][j]);
@@ -68,34 +68,60 @@ public:
 		}
 		return temp;
 	}
+	matrix operator * (T const &obj) {
+		matrix temp(I.size(), I[0].size(), 0);
+		for (int i = 0; i < I.size(); i++)
+		{
+			for (int j = 0; j < I[0].size(); j++)
+			{
+				temp.I[i][j] = I[i][j] * obj;
+			}
+		}
+		return temp;
+	}
+	matrix operator % (T const &obj) {
+		matrix temp=I;
+		for (int i = 0; i < I.size(); i++)
+		{
+			for (int j = 0; j < I[0].size(); j++)
+			{
+				temp.I[i][j] = temp.I[i][j] % obj;
+			}
+		}
+		return temp;
+	}
 };
 
+
+
 template<class T>
-matrix<T> pow(matrix<T> a, ull b)
+matrix<T> pow(matrix<T> a, long long b, long long mod)
 {
-	if (!b)return matrix<T>(a.I.size());
-	matrix<T> p = pow(a, b >> 1);
-	matrix<T> q = (p * p);
-	if (b & 1)return q * a;
+	if (!b)return matrix<T>(a.I.size())%mod;
+	matrix<T> p = pow(a, b >> 1,mod);
+	matrix<T> q = (p * p)%mod;
+	if (b & 1)return (q * a)%mod;
 	return q;
 }
+
 
 int main(int argc, char const *argv[])
 {
 	int t;
-	scanf("%d", &t);
-	for (int I = 1; I <= t; I++)
+	scanf("%d",&t);
+	for(int I=1;I<=t;I++)
 	{
-		printf("Case %d: ", I);
-		ull a, b, c;
-		scanf("%llu %llu %llu", &a, &b, &c);
-		vector<vector<ull>> kk, k;
-		kk.push_back({a, 1});
-		kk.push_back({ -b, 0});
-		k.push_back({a, 2});
-		matrix<ull> ans(kk);
-		ans = pow(ans, c);
-		ans = matrix<ull>(k) * ans;
-		printf("%llu\n", ans.I[0][1]);
+		printf("Case %d: ",I);
+		int a,b,c,d,p=1;
+		scanf("%d %d %d %d",&a,&b,&c,&d);
+		matrix<int> arr,brr;
+		brr.I.push_back({b,a});
+		arr.I.push_back({1,1});
+		arr.I.push_back({1,0});
+		while(d--)p*=10;
+		arr=pow(arr,c,p);
+		arr=(brr*arr)%p;
+		printf("%d\n", arr.I[0][1]);
 	}
+	return 0;
 }
